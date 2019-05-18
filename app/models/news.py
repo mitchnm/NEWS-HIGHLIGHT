f289db2cbab5b7541365359bd1b71b0ece9f92e1
mitchnm <mitchndungu21@gmail.com>
2019-05-18 20:42:21 +0300

    craeting call

--- app/models/news.py
+++ app/models/news.py
@@ -9,4 +9,26 @@ class News:
         self.article = article
         self.image= 'https://image.tmdb.org/t/p/w500/'+ image
         
-        
+class Source:
+    '''
+    Movie class to define Movie Objects
+    '''
+
+    def __init__(self, id, name, description):
+        self.id =id
+        self.name = name
+        self.description = description
+
+class Articles:
+    '''
+    defines the articles objects
+    '''
+
+    def __init__(self, blue, id, title, author, description, urlToImage, publishedAt, url):
+        self.blue = blue
+        self.title = title
+        self.author = author
+        self.description = description
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.url = url
--- app/request.py
+++ app/request.py
@@ -5,3 +5,5 @@ from .models import news
 News = news.News
 
 api_key = app.config['NEWS_API_KEY']
+
+base_url = app.config['NEWS_API_BASE_URL'] 
