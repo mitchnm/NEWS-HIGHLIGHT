232a9a28369386a547d3d8a80c42fca87b4ac40e
mitchnm <mitchndungu21@gmail.com>
2019-05-19 13:58:37 +0300

    created and made navbar

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from .config import DevConfig
-
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
+bootstrap = Bootstrap(app)
 
 from app import views
