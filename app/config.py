1287efe25d8931394da096354c042d2256f37b51
mitchnm <mitchndungu21@gmail.com>
2019-05-19 21:58:07 +0300

    fixed issues

--- app/config.py
+++ app/config.py
@@ -7,6 +7,7 @@ class Config:
     NEWS_SOURCES_BASE_URL = 'https://newsapi.org/v1/sources?language=en&category={}&apikey={}'
     NEWS_ARTICLES_BASE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 class ProdConfig(Config):
