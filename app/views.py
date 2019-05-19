b34f2adb0439be2dc6edd09b7bf540e4a6f3735b
mitchnm <mitchndungu21@gmail.com>
2019-05-19 22:11:55 +0300

    fixed

--- app/views.py
+++ app/views.py
@@ -8,39 +8,28 @@ from .request import get_sources, get_articles, search_articles
 @app.route('/')
 def index():
     '''
-    View root page function that returns the index page and its data.
+    View root page.
     ''' 
 
     title = 'Home - NewsApp'
 
-    #Getting Science Sources
-    science_sources = get_sources('science')
-
-    #Getting Business Sources
     business_sources = get_sources('business')
 
-    #Getting Entertainment Sources
     entertainment_sources = get_sources('entertainment')
 
-    #Getting General Sources
     general_sources = get_sources('general')
 
-    #Getting Health Sources
-    health_sources = get_sources('health')
-
-    #Getting Sports Sources
     sports_sources = get_sources('sports')
 
-    #Getting Technology Sources
     technology_sources = get_sources('technology')
 
-    return render_template('index.html', title=title, science=science_sources, business=business_sources, entertainment=entertainment_sources, sports=sports_sources, health=health_sources, general=general_sources, technology=technology_sources)
+    return render_template('index.html', title=title, business=business_sources, entertainment=entertainment_sources, sports=sports_sources, general=general_sources, technology=technology_sources)
 
 
 @app.route('/source/<id>')
 def source(id):
     '''
-    View source page function that returns the source and its articles.
+    View source page.
     '''
     all_articles = get_articles(id)
     title = f'NewsApp -- {id.upper()}'
